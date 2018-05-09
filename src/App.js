ee7f4e355aa055259144c09898893e54f8bcdec6
nendonerd <wang3721.2011@gmail.com>
2018-05-09 12:43:17 +0800

    Add some list
    aaaaaaa

--- src/App.js
+++ src/App.js
@@ -6,13 +6,40 @@ let defaultStyle = {
   'color': 'white'
 }
 
+
+
 class Aggregate extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ ...defaultStyle, 'display': 'inline-block', 'margin': '10px'}}>
         <h2>Number Text</h2>
       </div>
     );
+  }
+}
+
+class Input extends Component {
+  render() {
+    return (
+      <div className='Input'>
+        <input type="text"/>
+      </div>
+    )
+  }
+}
+
+class List extends Component {
+  render() {
+    return (
+      <div className='List' style={{...defaultStyle, 'display': 'inline-block', 'margin': '10px'}}>
+        Playlist Name
+        <ul>
+          <li>Song1</li>
+          <li>Song2</li>
+          <li>Song3</li>
+        </ul>
+      </div>
+    )
   }
 }
 
@@ -22,6 +49,12 @@ class App extends Component {
       <div className="App">
         <h1 style={{...defaultStyle, 'font-size': '5em'}}>Title</h1>
         <Aggregate />
+        <Aggregate />
+        <Input />
+        <List />
+        <List />
+        <List />
+        <List />
       </div>
     );
   }
